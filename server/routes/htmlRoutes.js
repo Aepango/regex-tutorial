c4e232986c647dacfe99478b5e24e1dacdb11594
Anne <aepanagotopulos@gmail.com>
2023-02-20 21:39:45 -0500

    changes to html and bug fixes

---
+++ server/routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = (app) =>
+    app.get('/', (req, res) =>
+        res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+    );
+Footer
+Footer, navigation
+Terms
+Privacy
+Security
+Status
+Docs
